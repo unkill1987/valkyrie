91836176bb33899c036094c0d1c84c432ff62512
unkill <unkill1987@naver.com>
2018-12-24 14:37:28 +0900

    2018-11-30

--- app/views.py
+++ app/views.py
@@ -1,14 +1,8 @@
-from tkinter.messagebox import showerror, showinfo
-from tkinter import *
 
 import requests
-from django.contrib import messages
-from django.core.mail import send_mail
-from django.template import RequestContext
-from pylab import *
 from django.core.paginator import Paginator, PageNotAnInteger, EmptyPage
 from django.http import HttpResponse, JsonResponse
-from django.shortcuts import render, redirect, render_to_response
+from django.shortcuts import render, redirect
 import hashlib
 
 import os
