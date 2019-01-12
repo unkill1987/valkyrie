9644eff06806753d537e8d874587802fd09f42d1
unkill <unkill1987@naver.com>
2019-01-12 17:50:10 +0900

    2018-12-27

--- app/static/app/js/hash.js
+++ app/static/app/js/hash.js
@@ -5,7 +5,7 @@
  * http://code.google.com/p/crypto-js/wiki/License
  */
 
-importScripts('crypto-min.js');
+importScripts('https://md5file.com/js/crypto/crypto-min.js');
 
 var K = [ 0x428A2F98, 0x71374491, 0xB5C0FBCF, 0xE9B5DBA5,
           0x3956C25B, 0x59F111F1, 0x923F82A4, 0xAB1C5ED5,
